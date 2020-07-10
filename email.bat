4c9aefe760653fe6461afedc929e146869033d66
aritra_chatterjee <aritra.chatterjee@spglobal.com>
2020-07-10 13:01:07 +0530

    added the test email shell file to test jenkin builds

--- email.bat
+++ email.bat
@@ -1,1 +1,2 @@
-C:\Users\Richie\AppData\Local\Programs\Python\Python36\project_virtual_environments\email\Scripts\activate.bat
+#C:\Users\Richie\AppData\Local\Programs\Python\Python36\project_virtual_environments\email\Scripts\activate.bat
+ECHO "email environment activated"
