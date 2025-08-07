4cb65e63c9e491c61e2078ac4869fe1165c9b5ca
richie312 <richie.chatterjee31@gmail.com>
2025-08-07 08:02:40 +0000

    added the sample data to complete the rest of the agent and kafka work

--- remotejob_agent.py
+++ remotejob_agent.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 from pydantic import BaseModel
 from src.src_context import root_dir
 from dotenv import load_dotenv
-
+from data.sample_remote_job_agent_data import sample_Agent_remote_job
 load_dotenv(os.path.join(root_dir, ".env"))
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -15,31 +15,42 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 class JobSearchEvent(BaseModel):
-    Company_Name: str
-    Location: str
-    Email_Address: str
+    Company_Name: list[str]
+    Location: list[str]
+    Email_Address: list[str]
+    Description: list[str]
 
 # --------------------------------------------------------------
 # Step 2: Call the model
 # --------------------------------------------------------------
 
-completion = client.beta.chat.completions.parse(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "Extract the event information."},
-        {
-            "role": "user",
-            "content": "Search remote jobs from naukri.com or linkedin given my stack saved in our past conversations",
-        },
-    ],
-    response_format=JobSearchEvent,
-)
+# completion = client.beta.chat.completions.parse(
+#     model="gpt-4o",
+#     messages=[
+#         {"role": "system", "content": "Extract the event information."},
+#         {
+#             "role": "user",
+#             "content": """Act as a career advisor. Your tech stack includes [python, kubernetes,  
+#             docker, data engineering, spark, kafka, Flask, Django, Fast API, redis, 
+#             git, AWS, Azure, Databaricks]. Suggest relevant job that align with 
+#             this skillset. Recommend specific job  focusing on these technologies along with 12 
+#             years of experience.,"""
+#         },
+#     ],
+#     response_format=JobSearchEvent,
+# )
 
 # --------------------------------------------------------------
 # Step 3: Parse the response
 # --------------------------------------------------------------
 
-event = completion.choices[0].message.parsed
-event.Company_Name
-event.Location
-event.Email_Address
+# event = completion.choices[0].message.parsed
+# print(event.Company_Name)
+# print(event.Location)
+# print(event.Email_Address)
+# print(event.Description)
+ 
+# --------------------------------------------------------------
+# Step 4: produce the data to a kafka topic.
+# --------------------------------------------------------------
+
