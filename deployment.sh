454ffd80f170e5900d13e10d7992aaddfb66556f
richie312 <richie.chatterjee31@gmail.com>
2025-08-31 16:47:10 +0530

    rmoved the deployment.sh file

--- deployment.sh
+++ deployment.sh
@@ -6,3 +6,15 @@ docker cp /home/pi/docker_vol_backup/dump.sql mysql:/dump.sql
 docker exec -it mysql bash -c "mysql -u root -p -e 'CREATE DATABASE ContainerDatabase; USE ContainerDatabase; SOURCE /dump.sql;'"
 
 docker run --network=my_rasp_network -p 5001:5001 richie31/notify
+
+# New command
+
+docker run --network=my_rasp_network \
+  -v /home/pi/mysql-init:/var/lib/mysql \
+  -v /home/pi/docker_vol_backup/2025-08-31/:/docker-entrypoint-initdb.d \
+  -p 3306:3306 \
+  -h 127.0.0.1 \
+  --name mysql \
+  -e MYSQL_ROOT_PASSWORD=Nirvikalpa#123 \
+  --restart unless-stopped \
+  mysql
